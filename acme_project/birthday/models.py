a4b21dd665aa16ec3015257b7dd6b10359c975e6
Sergey Mukhametzhanov <mukhametzhanov.sergey@ya.ru>
2024-05-12 00:45:34 +0300

    9.2.10

--- acme_project/birthday/models.py
+++ acme_project/birthday/models.py
@@ -11,6 +11,7 @@ class Birthday(models.Model):
 
     )
     birthday = models.DateField('Дата рождения', validators=(real_age,))
+    image = models.ImageField('Фото', upload_to='birthdays_images', blank=True)
 
     class Meta:
         constraints = (
