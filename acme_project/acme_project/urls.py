d8892e1953c99ca1cd6f9667bad9689b4b4f0217
Sergey Mukhametzhanov <mukhametzhanov.sergey@ya.ru>
2024-05-15 19:39:27 +0300

    10.1.4

--- acme_project/acme_project/urls.py
+++ acme_project/acme_project/urls.py
@@ -7,6 +7,8 @@ from django.urls import include, path
 
 urlpatterns = [
     path('', include('pages.urls')),
+    # Подключаем urls.py приложения для работы с пользователями.
+    path('auth/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
     path('birthday/', include('birthday.urls')),
     # В конце добавляем к списку вызов функции static.
